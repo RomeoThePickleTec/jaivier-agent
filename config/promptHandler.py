44b66eb808d9be9f976a5c3bc234efc3f55f8826
RomeoThePickleTec <a01637089@tec.mx>
2025-06-04 19:11:29 -0600

    Algo de documentación

---
+++ config/promptHandler.py
@@ -0,0 +1,7 @@
+"""
+En este archivo vamos a configurar la creación de los prompts. Los vamos a moldear, pero se envían desde el archivo principal (app.py)
+
+También aquí se va a realizar la llamada a la base 
+de datos para que se excluyan los contenidos de los 
+mensajes y no se mezclen con mensajes de otras personas.
+"""
