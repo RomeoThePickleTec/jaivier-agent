25e8776460118631df0cf9687177c7a94877d965
RomeoThePickleTec <a01637089@tec.mx>
2025-06-04 19:13:36 -0600

    Algo de documentación

--- config/promptHandler.py
+++ config/promptHandler.py
@@ -5,3 +5,14 @@ También aquí se va a realizar la llamada a la base
 de datos para que se excluyan los contenidos de los 
 mensajes y no se mezclen con mensajes de otras personas.
 """
+
+#Obtener "contexto del usuario que está enviando el mensaje"
+
+
+#Formular el prompt para indicarle el formato que esperamos recibir
+
+
+#Adjuntar las respuestas en JSON para que se agreguen al prompt
+
+
+#Enviar el prompt
